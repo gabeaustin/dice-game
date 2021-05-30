7e8d8c08a70d8cae241b1f1d09346ab09d642e0a
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-05-29 22:13:18 -0500

    added font awesome cdn and added dice on the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import "./App.css";
+import Die from "./Die";
 
 function App() {
   return (
     <div className="App">
-      
+      <Die face="five" />
+      <Die face="six" />
+      <Die face="three" />
+      <Die face="two" />
     </div>
   );
 }
